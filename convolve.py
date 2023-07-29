0d7ed7942fdd518259bb66c44991cba738c5587d
piyer2007 <129233218+piyer2007@users.noreply.github.com>
2023-07-29 19:54:39 -0400

    Add files via upload

--- convolve.py
+++ convolve.py
@@ -1,3 +1,281 @@
+BWSI Autonomous RACECAR
+Summer 2023
+Stream
+Classwork
+People
+BWSI Autonomous RACECAR
+Summer 2023
+Upcoming
+Due Yesterday
+11:59 PM – Code Clash #13
+Due tomorrow
+11:59 PM – Lab 5c Reflections (Sensor Fusion & Localization Algorithms)
+11:59 PM – Lab 4b Reflections (RACECAR Physics and IMU)
+
+Announce something to your class
+Assignment: "Lab 5c Reflections (Sensor Fusion & Localization Algorithms)"
+Chris Lai posted a new assignment: Lab 5c Reflections (Sensor Fusion & Localization Algorithms)
+Created YesterdayYesterday
+Material: "[7/28/2023] Sensor Fusion & Localization Algorithms"
+Chris Lai posted a new material: [7/28/2023] Sensor Fusion & Localization Algorithms
+Created YesterdayYesterday
+Material: "Lecture 12 - Sensor Fusion and Localization Algorithms"
+Chris Lai posted a new material: Lecture 12 - Sensor Fusion and Localization Algorithms
+Created YesterdayYesterday
+
+Announcement: "Code clash - Convolution solution For…"
+Paul Johnson
+Created YesterdayYesterday
+Code clash - Convolution solution
+For those unfamiliar with probability densities and cumulative distribution functions, or numerical integration
+
+convolve_CC_Run.py
+Text
+
+Add class comment…
+
+
+Announcement: "Task Planning with PERT chart"
+Paul Johnson
+Created YesterdayYesterday
+Task Planning with PERT chart
+
+pert_run.py
+Text
+
+pert_test.txt
+Text
+
+Add class comment…
+
+Assignment: "Code Clash #13"
+Chris Lai posted a new assignment: Code Clash #13
+Created YesterdayYesterday
+Material: "Kalman Filter Lecture (RACECAR Summer 2022)"
+Chris Lai posted a new material: Kalman Filter Lecture (RACECAR Summer 2022)
+Created Jul 27Jul 27
+Assignment: "Lab 5b Reflections (Advanced Robotics & Prediction Algorithms)"
+Chris Lai posted a new assignment: Lab 5b Reflections (Advanced Robotics & Prediction Algorithms)
+Created Jul 27Jul 27
+Material: "[7/27/2023] CNN, Lipo Battery Safety"
+Chris Lai posted a new material: [7/27/2023] CNN, Lipo Battery Safety
+Created Jul 27Jul 27
+Material: "[7/26/2023] Fiducial Markers"
+Chris Lai posted a new material: [7/26/2023] Fiducial Markers
+Created Jul 27Jul 27
+Material: "Lecture 11 - Advanced Robotics & Prediction Algorithms"
+Chris Lai posted a new material: Lecture 11 - Advanced Robotics & Prediction Algorithms
+Created Jul 27Jul 27
+Assignment: "Code Clash #12"
+Chris Lai posted a new assignment: Code Clash #12
+Created Jul 27Jul 27
+Assignment: "Lab 5a Reflections (Fiducial Markers)"
+Chris Lai posted a new assignment: Lab 5a Reflections (Fiducial Markers)
+Created Jul 26Jul 26
+1 class comment
+Material: "Lecture 10 - Fiducial Markers"
+Chris Lai posted a new material: Lecture 10 - Fiducial Markers
+Created Jul 26Jul 26
+Material: "Lab 5 (Updated 7/26/2023)"
+Chris Lai posted a new material: Lab 5 (Updated 7/26/2023)
+Created Jul 26Jul 26
+Assignment: "Code Clash #11"
+Chris Lai posted a new assignment: Code Clash #11
+Created Jul 26Jul 26
+Material: "[7/25/2023] RACECAR Physics and IMU"
+Chris Lai posted a new material: [7/25/2023] RACECAR Physics and IMU
+Created Jul 26Jul 26
+Material: "Lecture 9 - Physics and IMU"
+Chris Lai posted a new material: Lecture 9 - Physics and IMU
+Created Jul 25Jul 25
+Assignment: "Lab 4b Reflections (RACECAR Physics and IMU)"
+Chris Lai posted a new assignment: Lab 4b Reflections (RACECAR Physics and IMU)
+Created Jul 25Jul 25
+1 class comment
+Material: "Week 2 Race (Cone Slalom) Raw Footage"
+Chris Lai posted a new material: Week 2 Race (Cone Slalom) Raw Footage
+Created Jul 25Jul 25
+Assignment: "Code Clash #10"
+Koneshka Dey posted a new assignment: Code Clash #10
+Created Jul 25Jul 25
+Material: "RACECAR-MN Library Documentation"
+Chris Lai posted a new material: RACECAR-MN Library Documentation
+Created Jul 24Jul 24 (Edited Jul 25)
+Material: "[7/24/2023] The LIDAR Sensor"
+Chris Lai posted a new material: [7/24/2023] The LIDAR Sensor
+Created Jul 24Jul 24
+Material: "Lecture 8 - The LIDAR Sensor"
+Chris Lai posted a new material: Lecture 8 - The LIDAR Sensor
+Created Jul 24Jul 24
+Assignment: "Lab 4a Reflections (LIDAR Data Processing)"
+Chris Lai posted a new assignment: Lab 4a Reflections (LIDAR Data Processing)
+Created Jul 24Jul 24
+Material: "Christopher Watson - Starting With AI"
+Chris Lai posted a new material: Christopher Watson - Starting With AI
+Created Jul 24Jul 24
+Material: "Chris Watson Bio Questions"
+Chris Lai posted a new material: Chris Watson Bio Questions
+Created Jul 24Jul 24
+Material: "Lab 4 (Updated 7/24/2023)"
+Chris Lai posted a new material: Lab 4 (Updated 7/24/2023)
+Created Jul 24Jul 24
+Assignment: "Code Clash #9"
+Chris Lai posted a new assignment: Code Clash #9
+Created Jul 24Jul 24 (Edited Jul 24)
+Assignment: "Cone Slalom Race Reflections (Day 2)"
+Chris Lai posted a new assignment: Cone Slalom Race Reflections (Day 2)
+Created Jul 21Jul 21
+Assignment: "Code Clash #8"
+Chris Lai posted a new assignment: Code Clash #8
+Created Jul 21Jul 21
+Assignment: "Cone Slalom Race Reflections (Day 1)"
+Chris Lai posted a new assignment: Cone Slalom Race Reflections (Day 1)
+Created Jul 20Jul 20
+Material: "[7/19/2023] 2D Convolutions, Probability Density Functions "
+Chris Lai posted a new material: [7/19/2023] 2D Convolutions, Probability Density Functions
+Created Jul 20Jul 20
+Material: "Lecture 7 - Cone Slalom"
+Chris Lai posted a new material: Lecture 7 - Cone Slalom
+Created Jul 20Jul 20
+Assignment: "Code Clash #7"
+Chris Lai posted a new assignment: Code Clash #7
+Created Jul 20Jul 20
+Assignment: "Lab 3c Reflections (Convolution and Image Processing)"
+Chris Lai posted a new assignment: Lab 3c Reflections (Convolution and Image Processing)
+Created Jul 19Jul 19 (Edited Jul 19)
+Assignment: "Code Clash #6"
+Chris Lai posted a new assignment: Code Clash #6
+Created Jul 19Jul 19
+Assignment: "Lab 3b Reflections (Depth Camera Cone Parking)"
+Chris Lai posted a new assignment: Lab 3b Reflections (Depth Camera Cone Parking)
+Created Jul 18Jul 18 (Edited Jul 19)
+Material: "Alla Gimbel Bio Questions"
+Chris Lai posted a new material: Alla Gimbel Bio Questions
+Created Jul 18Jul 18 (Edited Jul 24)
+Material: "[7/18/2023] Depth Camera Review & Intro to Convolution"
+Chris Lai posted a new material: [7/18/2023] Depth Camera Review & Intro to Convolution
+Created Jul 18Jul 18
+Material: "Lecture 6 - Advanced Sensor Utility Techniques"
+Chris Lai posted a new material: Lecture 6 - Advanced Sensor Utility Techniques
+Created Jul 18Jul 18
+Assignment: "Code Clash #5"
+Ali Qattan posted a new assignment: Code Clash #5
+Created Jul 18Jul 18
+Material: "[7/17/2023] Intro to Depth Camera"
+Chris Lai posted a new material: [7/17/2023] Intro to Depth Camera
+Created Jul 17Jul 17
+Assignment: "Lab 3a Reflections (Depth Camera Safety Stop)"
+Chris Lai posted a new assignment: Lab 3a Reflections (Depth Camera Safety Stop)
+Created Jul 17Jul 17 (Edited Jul 17)
+Material: "Lecture 5 - Intro to Depth Camera, Null Values"
+Chris Lai posted a new material: Lecture 5 - Intro to Depth Camera, Null Values
+Created Jul 17Jul 17
+Assignment: "Code Clash #4"
+Chris Lai posted a new assignment: Code Clash #4
+Created Jul 17Jul 17
+Material: "[7/14/2023] State Machines & Enumeration"
+Chris Lai posted a new material: [7/14/2023] State Machines & Enumeration
+Created Jul 14Jul 14
+Assignment: "Lab 2b Reflections (Line Blitz)"
+Chris Lai posted a new assignment: Lab 2b Reflections (Line Blitz)
+Created Jul 14Jul 14
+Material: "Lecture 4 - State Machines, Enumeration"
+Chris Lai posted a new material: Lecture 4 - State Machines, Enumeration
+Created Jul 14Jul 14
+Assignment: "Code Clash #3"
+Chris Lai posted a new assignment: Code Clash #3
+Created Jul 14Jul 14
+Material: "[7/13/2023] Advanced Control Theory & Computer Vision"
+Chris Lai posted a new material: [7/13/2023] Advanced Control Theory & Computer Vision
+Created Jul 13Jul 13
+
+Announcement: "Hi class, On #3 (PID Controller…"
+Chris Lai
+Created Jul 13Jul 13
+Hi class,
+
+On #3 (PID Controller problem) on today's reflections, be sure to show your work. If you used code to solve the problem, be sure to explain your code. In addition, please state if you worked on this question by yourself or with your team. Otherwise, you will not receive credit for the problem. Thanks!
+
+Add class comment…
+
+Assignment: "Lab 2a Reflections (Line Following)"
+Chris Lai posted a new assignment: Lab 2a Reflections (Line Following)
+Created Jul 13Jul 13
+Material: "Lecture 3 - Advanced Control Theory, Intro to Computer Vision"
+Chris Lai posted a new material: Lecture 3 - Advanced Control Theory, Intro to Computer Vision
+Created Jul 13Jul 13
+Material: "Lab 2 (Updated 7/13/2023)"
+Chris Lai posted a new material: Lab 2 (Updated 7/13/2023)
+Created Jul 13Jul 13
+Assignment: "Code Clash #2"
+Chris Lai posted a new assignment: Code Clash #2
+Created Jul 13Jul 13
+Material: "[7/12/2023] Timing Paradigms, Control Theory, Data Structures"
+Chris Lai posted a new material: [7/12/2023] Timing Paradigms, Control Theory, Data Structures
+Created Jul 13Jul 13
+Material: "Lecture 2 - Timing Paradigms, Control Theory, Data Structures"
+Chris Lai posted a new material: Lecture 2 - Timing Paradigms, Control Theory, Data Structures
+Created Jul 12Jul 12
+Assignment: "Lab 1 Reflections (Driving)"
+Chris Lai posted a new assignment: Lab 1 Reflections (Driving)
+Created Jul 12Jul 12
+Assignment: "Code Clash #1"
+Chris Lai posted a new assignment: Code Clash #1
+Created Jul 12Jul 12
+
+Announcement: "Class, Lab Reflections #0 (RACECAR…"
+Chris Lai
+Created Jul 11Jul 11
+Class,
+
+Lab Reflections #0 (RACECAR Setup) has been assigned. The lecture recording from today has also been posted in case you would like to review it. If you have any questions, please let us know.
+
+See you tomorrow!
+
+Add class comment…
+
+Material: "[7/11/2023] Hardware and Software Setup"
+Chris Lai posted a new material: [7/11/2023] Hardware and Software Setup
+Created Jul 11Jul 11
+Assignment: "Lab 0 Reflections (RACECAR Setup)"
+Chris Lai posted a new assignment: Lab 0 Reflections (RACECAR Setup)
+Created Jul 11Jul 11
+Material: "Lecture 1 - Hardware and Software Setup"
+Chris Lai posted a new material: Lecture 1 - Hardware and Software Setup
+Created Jul 11Jul 11
+Material: "RACECAR-MN Robot Software Installation Guide"
+Koneshka Dey posted a new material: RACECAR-MN Robot Software Installation Guide
+Created Jul 11Jul 11
+Material: "RACECAR-MN Class Files (Summer 2023)"
+Chris Lai posted a new material: RACECAR-MN Class Files (Summer 2023)
+Created Jul 11Jul 11
+Material: "RACECAR-MN PC Software Installation Guide"
+Chris Lai posted a new material: RACECAR-MN PC Software Installation Guide
+Created Jul 11Jul 11
+Material: "[7/10/2023] Welcome to BWSI RACECAR!"
+Chris Lai posted a new material: [7/10/2023] Welcome to BWSI RACECAR!
+Created Jul 10Jul 10
+
+Announcement: "Hey, just wanted to inform everyone…"
+Koneshka Dey
+Created Jul 10Jul 10
+Hey, just wanted to inform everyone that Code Clash #0 has been assigned. The deadline for this assignment is tonight at 11:59 pm so please ensure that you complete them. Looking forward to seeing everyone tomorrow!
+
+Add class comment…
+
+Material: "Course Syllabus"
+Chris Lai posted a new material: Course Syllabus
+Created Jul 10Jul 10
+Material: "Lecture 0 - Welcome to BWSI RACECAR!"
+Chris Lai posted a new material: Lecture 0 - Welcome to BWSI RACECAR!
+Created Jul 10Jul 10
+Assignment: "Intro Reflections"
+Chris Lai posted a new assignment: Intro Reflections
+Created Jul 10Jul 10
+Assignment: "Code Clash #0"
+Chris Lai posted a new assignment: Code Clash #0
+Created Jul 10Jul 10
+BWSI Autonomous RACECAR Summer 2023
 """
 Copyright MIT BWSI Autonomous RACECAR Course
 MIT License
@@ -5,10 +283,9 @@ Summer 2023
 
 Code Clash #9 - Racecar code design (convolve.py)
 
-
 Author: Paul Johnson
 
-Difficulty Level: 7/10
+Difficulty Level: 9/10
 
 Prompt: You’ve made it to a mini Grand Prix sprint race with only two obstacles,
 and you’ve only had enough time to test each obstacle separately in your labs.
@@ -18,19 +295,19 @@ You want to be 90% confident that one code design is better than another for the
 
 See the background below for the tools you will use to figure this out. In the prompt below data
 
-Background: Exponential, uniform probability distributions and convolutions
+Background: Exponential, uniform probability densities and convolutions
 
 1) Exponential distribution - constant times
 - An exponential probability distribution is used to find the distribution of
 times around an average time until an event. It requires an input of the average
 event rate per second: r (events/second), and outputs a measure of probability density.
 
-- It’s probability density function (PDF) is f(t) = r  exp (-r t) and PDF literally says:
+- It’s probability density function (PDF) is f(t) = r * exp(-r t) and PDF literally says:
 The probability of an event occurring between [t, t+t] is the event rate per second r,
-times the probability that an event has not occurred yet exp( -r t), known as the survival function.
+times the probability that an event has not occurred yet before time t: e*(-r t).
 
 2) Uniform distribution - widely distributed times
-A uniform distribution has many applications when any value is expected between t[a,b] with
+A uniform distribution has many applications when any value is expected between t in[a,b] with
 no clear preferred value.
 Statistical methods exist beyond the scope of this problem to identify if a data set belongs to
 a symmetrical uniform and normal distributions.
@@ -42,7 +319,16 @@ Given the prompt below, complete the convolution integral for the equation given
 Prompt
 [TODO] #1 Complete the convolution integral for design , validate the test case.
 [TODO] #2 Implement a search algorithm given probModelAX to determine the time to completion (timeModelAX) with 90% probability of confidence.
+
+Test Cases:
+Shown: dict =  {timesObs1AX: [9, 20, 10, 25, 14, 22, 10, 18] # Test Uniform
+        	   timesObs2AX:  [4, 4, 4, 4, 4, 4, 4],                 # Test Exponential
+	   probModelA: 0.50,                                        # Test confidence 50%
+	   timeModelA:  14}                                             # In seconds
+
 """
+
+import matplotlib.pyplot as plt
 
 class DataInput:
     def __init__(self):
@@ -54,7 +340,7 @@ class DataInput:
         self.T = 50
         self.ti = [t for t in range(self.T)]
 
-class Solution:
+class DataAnalysis:
     def __init__(self, data):
         self.data = data
         self.f1A = [self.uniformPDF(fti, min(self.data.timesObs1A), max(self.data.timesObs1A)) for fti in self.data.ti]
@@ -62,7 +348,7 @@ class Solution:
         self.convolutionA = self.compute_convolution()
         self.mean1A = self.data.mean1A
         self.mean2A = self.data.mean2A
-        self.timeModelA = self.search_ppf(self.compute_cdf(), self.data.probModel, epsilon=1e-4)
+        self.timeModelA = self.search_ppf(self.compute_cdf_convolution(), self.data.probModel, epsilon=1e-4)
 
     def exp(self, x, terms=100):
         """Compute the exponential function
@@ -133,34 +419,12 @@ class Solution:
             convolutionA.append(self.integrateTrapz(Cj, self.data.ti[:i+1]))
         return convolutionA
 
-
-        # ...
-        return convolutionA
-
-    def compute_cdf(self):
+    def compute_cdf_convolution(self):
         A = self.integrateTrapz(self.convolutionA, self.data.ti)
         self.convolutionA = [self.convolutionA[i]/A for i in range(len(self.convolutionA))]
         return [self.integrateTrapz(self.convolutionA[:i], self.data.ti[:i]) for i in self.data.ti[1:]]
 
-
-    def search_ppf(cdf_values, target, epsilon=1e-6):
-        """
-        Calculate the PPF (point percent function = inverse cuumulative distribution function [CDF])
-        of a probability distribution using search.
-
-        This will find the X axis value of a given y axis value input
-
-        cdf_values (list): A sorted list representing the CDF from 0 to 1.
-        target (float): The target probability for which the PPF is computed.
-        epsilon (float): The tolerance level for the search.
-
-        return (float): The PPF of the probability distribution.
-        """
-
-        """
-        [TODO] (2) Implement search function here
-        """
-
+    def search_ppf(self, cdf_values, target, epsilon=1e-6):
         lo = 0
         hi = len(cdf_values) - 1
         while hi - lo > 1:
@@ -174,16 +438,39 @@ class Solution:
         else:
             return lo
 
+class OutputPlotting:
+    def __init__(self, data):
+        self.data = data
 
+    def plot(self):
+        fig, ax1 = plt.subplots()
+        ax2 = ax1.twinx()
+        ax1.step(self.data.data.ti, self.data.f1A,label="Probability Density f1A")
+        ax1.step(self.data.data.ti, self.data.f2A, '--',label="Probability Density f2A")
+        ax1.plot(self.data.data.ti, self.data.convolutionA,'-g',label="Convolution(f1A,f2A)")
+        ax2.plot(self.data.data.ti[1:], self.data.compute_cdf_convolution(),'-.g', label="Convolution A CDF Pr(t'<t)")
+        ax1.set_xlabel("Time: $(s)$")
+        ax1.set_ylabel("Probability Density $(1/s)$")
+        ax2.set_ylabel("Cummulative Distribution Probability(t' < t) ")
+        ax1.set_xlim([0,30])
+        ax1.legend(loc='center right')
+        ax2.legend(loc='lower right')
+        ax1.grid()
+        fig.tight_layout()
+        plt.show()
 
 def main():
     data_input = DataInput()
-    data_analysis = Solution(data_input)
+    data_analysis = DataAnalysis(data_input)
     print(f"mean1A = {round(data_analysis.mean1A,2)}")
     print(f"mean2A = {round(data_analysis.mean2A,2)}")
     print(f"mean1A + mean2A = {round(data_analysis.mean1A + data_analysis.mean2A,2)}")
     print("The probability that two events will take less than t' < t:")
     print("PrA(t'< {:2.1f} s) = {:2.2f}".format(data_analysis.timeModelA, data_input.probModel))
+    output_plot = OutputPlotting(data_analysis)
+    output_plot.plot()
 
 if __name__ == "__main__":
     main()
+convolve_CC_Run.py
+Displaying convolve_CC_Run.py.
